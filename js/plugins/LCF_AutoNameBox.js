f3f5c0d42a605a25027c36ebbbc4f9d089d27f2c
ProfDetermined <a2z.team.198@gmail.com>
2018-04-11 21:44:41 +0700

    Add files via upload

---
+++ js/plugins/LCF_AutoNameBox.js
@@ -0,0 +1,282 @@
+/*:
+ * @plugindesc ต้องการ Plugin YEP_MessageCore ด้วยนะ
+ * @author Lucifer Thailand
+ * 
+ * @param == ใส่ชื่อ 1 ==
+ * @default
+ * 
+ * @param Text Code 1
+ * @desc ให้ใส่ \N<ชื่อตัวละคร>
+ * @default \N<???>
+ * 
+ * @param File 1
+ * @desc ให้ใส่ชื่อของไฟล์ภาพตัวละครใน (located in /img/faces/).
+ * @default Actor1
+ * 
+ * @param Indexes 1
+ * @desc ไม่ต้องแก้นะ (ป้องกันไว้ก่อน)
+ * @default 0, 1, 2, 3, 4, 5, 6, 7
+ * 
+ * @param == Auto Name 2 ==
+ * @default
+ * 
+ * @param Text Code 2
+ * @desc ให้ใส่ \N<ชื่อตัวละคร>
+ * @default
+ * 
+ * @param File 2
+ * @desc ให้ใส่ชื่อของไฟล์ภาพตัวละครใน (located in /img/faces/).
+ * @default
+ * 
+ * @param Indexes 2
+ * @desc ไม่ต้องแก้นะ (ป้องกันไว้ก่อน)
+ * @default 0, 1, 2, 3, 4, 5, 6, 7
+ * 
+ * @param == Auto Name 3 ==
+ * @default
+ * 
+ * @param Text Code 3
+ * @desc ให้ใส่ \N<ชื่อตัวละคร>
+ * @default
+ * 
+ * @param File 3
+ * @desc ให้ใส่ชื่อของไฟล์ภาพตัวละครใน (located in /img/faces/).
+ * @default
+ * 
+ * @param Indexes 3
+ * @desc ไม่ต้องแก้นะ (ป้องกันไว้ก่อน)
+ * @default 0, 1, 2, 3, 4, 5, 6, 7
+ * 
+ * @param == Auto Name 4 ==
+ * @default
+ * 
+ * @param Text Code 4
+ * @desc ให้ใส่ \N<ชื่อตัวละคร>
+ * @default
+ * 
+ * @param File 4
+ * @desc ให้ใส่ชื่อของไฟล์ภาพตัวละครใน (located in /img/faces/).
+ * @default
+ * 
+ * @param Indexes 4
+ * @desc ไม่ต้องแก้นะ (ป้องกันไว้ก่อน)
+ * @default 0, 1, 2, 3, 4, 5, 6, 7
+ * 
+ * @param == Auto Name 5 ==
+ * @default
+ * 
+ * @param Text Code 5
+ * @desc ให้ใส่ \N<ชื่อตัวละคร>
+ * @default
+ * 
+ * @param File 5
+ * @desc ให้ใส่ชื่อของไฟล์ภาพตัวละครใน (located in /img/faces/).
+ * @default
+ * 
+ * @param Indexes 5
+ * @desc ไม่ต้องแก้นะ (ป้องกันไว้ก่อน)
+ * @default 0, 1, 2, 3, 4, 5, 6, 7
+ * 
+ * @param == Auto Name 6 ==
+ * @default
+ * 
+ * @param Text Code 6
+ * @desc ให้ใส่ \N<ชื่อตัวละคร>
+ * @default
+ * 
+ * @param File 6
+ * @desc ให้ใส่ชื่อของไฟล์ภาพตัวละครใน (located in /img/faces/).
+ * @default
+ * 
+ * @param Indexes 6
+ * @desc ไม่ต้องแก้นะ (ป้องกันไว้ก่อน)
+ * @default 0, 1, 2, 3, 4, 5, 6, 7
+ * 
+ * @param == Auto Name 7 ==
+ * @default
+ * 
+ * @param Text Code 7
+ * @desc ให้ใส่ \N<ชื่อตัวละคร>
+ * @default
+ * 
+ * @param File 7
+ * @desc ให้ใส่ชื่อของไฟล์ภาพตัวละครใน (located in /img/faces/).
+ * @default
+ * 
+ * @param Indexes 7
+ * @desc ไม่ต้องแก้นะ (ป้องกันไว้ก่อน)
+ * @default 0, 1, 2, 3, 4, 5, 6, 7
+ * 
+ * @param == Auto Name 8 ==
+ * @default
+ * 
+ * @param Text Code 8
+ * @desc ให้ใส่ \N<ชื่อตัวละคร>
+ * @default
+ * 
+ * @param File 8
+ * @desc ให้ใส่ชื่อของไฟล์ภาพตัวละครใน (located in /img/faces/).
+ * @default
+ * 
+ * @param Indexes 8
+ * @desc ไม่ต้องแก้นะ (ป้องกันไว้ก่อน)
+ * @default 0, 1, 2, 3, 4, 5, 6, 7
+ * 
+ * @param == Auto Name 9 ==
+ * @default
+ * 
+ * @param Text Code 9
+ * @desc ให้ใส่ \N<ชื่อตัวละคร>
+ * @default
+ * 
+ * @param File 9
+ * @desc ให้ใส่ชื่อของไฟล์ภาพตัวละครใน (located in /img/faces/).
+ * @default
+ * 
+ * @param Indexes 9
+ * @desc ไม่ต้องแก้นะ (ป้องกันไว้ก่อน)
+ * @default 0, 1, 2, 3, 4, 5, 6, 7
+ * 
+ * @param == Auto Name 10 ==
+ * @default
+ * 
+ * @param Text Code 10
+ * @desc ให้ใส่ \N<ชื่อตัวละคร>
+ * @default
+ * 
+ * @param File 10
+ * @desc ให้ใส่ชื่อของไฟล์ภาพตัวละครใน (located in /img/faces/).
+ * @default
+ * 
+ * @param Indexes 10
+ * @desc ไม่ต้องแก้นะ (ป้องกันไว้ก่อน)
+ * @default 0, 1, 2, 3, 4, 5, 6, 7
+ * 
+ * @param == Auto Name 11 ==
+ * @default
+ * 
+ * @param Text Code 11
+ * @desc ให้ใส่ \N<ชื่อตัวละคร>
+ * @default
+ * 
+ * @param File 11
+ * @desc ให้ใส่ชื่อของไฟล์ภาพตัวละครใน (located in /img/faces/).
+ * @default
+ * 
+ * @param Indexes 11
+ * @desc ไม่ต้องแก้นะ (ป้องกันไว้ก่อน)
+ * @default 0, 1, 2, 3, 4, 5, 6, 7
+ * 
+ * @param == Auto Name 12 ==
+ * @default
+ * 
+ * @param Text Code 12
+ * @desc ให้ใส่ \N<ชื่อตัวละคร>
+ * @default
+ * 
+ * @param File 12
+ * @desc ให้ใส่ชื่อของไฟล์ภาพตัวละครใน (located in /img/faces/).
+ * @default
+ * 
+ * @param Indexes 12
+ * @desc ไม่ต้องแก้นะ (ป้องกันไว้ก่อน)
+ * @default 0, 1, 2, 3, 4, 5, 6, 7
+ * 
+ * @param == Auto Name 13 ==
+ * @default
+ * 
+ * @param Text Code 13
+ * @desc ให้ใส่ \N<ชื่อตัวละคร>
+ * @default
+ * 
+ * @param File 13
+ * @desc ให้ใส่ชื่อของไฟล์ภาพตัวละครใน (located in /img/faces/).
+ * @default
+ * 
+ * @param Indexes 13
+ * @desc ไม่ต้องแก้นะ (ป้องกันไว้ก่อน)
+ * @default 0, 1, 2, 3, 4, 5, 6, 7
+ * 
+ * @param == Auto Name 14 ==
+ * @default
+ * 
+ * @param Text Code 14
+ * @desc ให้ใส่ \N<ชื่อตัวละคร>
+ * @default
+ * 
+ * @param File 14
+ * @desc ให้ใส่ชื่อของไฟล์ภาพตัวละครใน (located in /img/faces/).
+ * @default
+ * 
+ * @param Indexes 14
+ * @desc ไม่ต้องแก้นะ (ป้องกันไว้ก่อน)
+ * @default 0, 1, 2, 3, 4, 5, 6, 7
+ * 
+ * @param == Auto Name 15 ==
+ * @default
+ * 
+ * @param Text Code 15
+ * @desc ให้ใส่ \N<ชื่อตัวละคร>
+ * @default
+ * 
+ * @param File 15
+ * @desc ให้ใส่ชื่อของไฟล์ภาพตัวละครใน (located in /img/faces/).
+ * @default
+ * 
+ * @param Indexes 15
+ * @desc ไม่ต้องแก้นะ (ป้องกันไว้ก่อน)
+ * @default 0, 1, 2, 3, 4, 5, 6, 7
+ 
+ *
+ * @help
+ *
+ * Auto Name Box
+ * Version 1.01
+ * Lucifer Thailand
+ */
+
+var LCF = LCF || {};
+LCF.AutoNameBox = LCF.AutoNameBox || {};
+
+var Imported = Imported || {};
+Imported["Lucifer Thailand (Yanfly Extension) Auto Name Box"] = true;
+
+if(Imported.YEP_MessageCore) {
+
+	(function(_) {
+
+		"use strict";
+
+		_.names = [];
+		_.align = [];
+		_.files = [];
+		_.index = [];
+		for(var i = 1; i <= 100; i++) {
+			_.names[i] = String(PluginManager.parameters('LCF_AutoNameBox')['Text Code ' + i]);
+			_.files[i] = String(PluginManager.parameters('LCF_AutoNameBox')['File ' + i]);
+			_.index[i] = String(PluginManager.parameters('LCF_AutoNameBox')['Indexes ' + i]).split(/\s*,\s*/);
+			for(var j = 0; j < _.index[i].length; j++) {
+				_.index[i][j] = parseInt(_.index[i][j]);
+			}
+		}
+
+		var _Game_Message_addText = Game_Message.prototype.addText;
+		Game_Message.prototype.addText = function(text) {
+			if(!text.match(/\\(?:N|N1|N2|N3|N4|N5|NC|NR|ND|ND1|ND2|ND3|ND4|ND5|NDC|NDR|NT|NT1|NT2|NT3|NT4|NT5|NTC|NTR)<(.*)>/i)) {
+				var correctIndex = 0;
+				for(var i = 0; i < _.files.length; i++) {
+					if(_.files[i] === this.faceName() && _.index[i].indexOf(this.faceIndex()) > -1) {
+						text = _.names[i] + text;
+						break;
+					}
+				}
+			}
+			_Game_Message_addText.call(this, text);
+		};
+
+
+	})(LCF.AutoNameBox);
+
+} else {
+	alert("Plugin 'LCF_AutoNameBox' ต้องการ Plugin 'YEP_MessageCore'. ให้ติดตั้งด้วย 'YEP_MessageCore' และ/หรือ เปลี่ยนลำดับ 'LCF_AutoNameBox' ไปอยู่ล่างสุด");
+}
